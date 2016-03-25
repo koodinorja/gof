7b98d991143087d8769818b2e9e2aeb08e80f600
Kasper Keso <cabakeso@gmail.com>
2016-03-25 23:39:37 +0200

    yeah

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,11 +1,9 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import gameOfLife from './gameOfLife/gameOfLife';
 
 let componentModule = angular.module('app.components', [
   Home.name,
-  About.name,
   gameOfLife.name
 ]);
 
